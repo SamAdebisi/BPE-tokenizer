432189af3816d2b0080a35e5dac5eb15c8c505ca
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-16 01:43:38 +0300

    Update base.py

--- minibpe/base.py
+++ minibpe/base.py
@@ -62,7 +62,7 @@ class Tokenizer:
     def _build_vocab(self):
         # vocab is simply and deterministically derived from merges 
         vocab = {idx: bytes([idx]) for idx in range(256)}
-         for (p0, p1), idx in self.merges.items():
+        for (p0, p1), idx in self.merges.items():
              vocab[idx] = vocab[p0] + vocab[p1]
         for 
          return vocab 
