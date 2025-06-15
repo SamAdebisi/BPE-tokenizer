69fd875298ad70b1a6a851ccdf97d75250f41431
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-16 01:46:00 +0300

    Update base.py

--- minibpe/base.py
+++ minibpe/base.py
@@ -64,5 +64,7 @@ class Tokenizer:
         vocab = {idx: bytes([idx]) for idx in range(256)}
         for (p0, p1), idx in self.merges.items():
             vocab[idx] = vocab[p0] + vocab[p1]
+        for special, idx in self.special_tokens.items():
+            vocab[idx] = special.encode("utf-8")
 
-         return vocab 
+        return vocab 
