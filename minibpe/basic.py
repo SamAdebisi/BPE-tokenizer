86d1dea577c565a73a6e2197191912b936994730
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-15 23:14:25 +0300

    Update basic.py

--- minibpe/basic.py
+++ minibpe/basic.py
@@ -1,3 +1,10 @@
     """
     Minimal (byte-level) Byte Pair Encoding tokenizer.
+    
+    Algorithmically follows along the GPT tokenizer:
+https://github.com/openai/gpt-2/blob/master/src/encoder.py
+
+But:
+- Does not handle the regular expression splitting pattern.
+- Does not handle any special tokens.
     """
