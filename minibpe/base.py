ea3447eb5d0b50fa279e8163b7b9bb658335adf3
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-15 22:39:17 +0300

    Update base.py

--- minibpe/base.py
+++ minibpe/base.py
@@ -6,3 +6,9 @@ import unicodedata
 
 
 def get_stats(ids, counts=None):
+    """_summary_
+
+    Args:
+        ids (_type_): _description_
+        counts (_type_, optional): _description_. Defaults to None.
+    """
