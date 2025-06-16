9f27e6f3f70502c7be2e8a53418d2802778cb6c9
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-16 11:01:34 +0300

    Update basic.py

--- minibpe/basic.py
+++ minibpe/basic.py
@@ -72,7 +72,7 @@ class BasicsicTokenizer(Tokenizer):
         return ids 
     
     def decode(self, ids):
-        text_bytes = b"".join([self.vocab[idx] for idx in ids])
+        text_bytes = b"".join(self.vocab[idx] for idx in ids)
         return text_bytes.decode("utf-8", errors="replace")
     
    
