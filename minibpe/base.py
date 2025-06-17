284d546075a1f91b2a0b7ad85647b1c0fa404fc9
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-17 16:57:10 +0300

    Update base.py

--- minibpe/base.py
+++ minibpe/base.py
@@ -94,6 +94,7 @@ class Tokenizer:
         with open(vocab_file, "w", encoding="utf-8") as f:
             for idx, token in self.vocab.items():
                 # note: many tokens may be partial utf-8 sequences 
+                # and cannot be decoded into valid strings. 
                 
             
 
