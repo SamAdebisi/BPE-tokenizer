73e8a2963ace5c8e55730d50701870af7aa6123f
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-17 16:13:51 +0300

    Update base.py

--- minibpe/base.py
+++ minibpe/base.py
@@ -78,7 +78,7 @@ class Tokenizer:
         # write the model: to be used in load() later 
         model_file = file_prefix + ".model" 
         with open(model_file, "w") as f:
-            # write the version, 
+            # write the version, pattern and merges, 
             f.write("version: 1\n")
 
         pass
