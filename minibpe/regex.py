9daaa19feb7fc419a7bb52c913cfb86aed2f7c7b
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-19 02:23:28 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -54,3 +54,4 @@ class RegexTokenizer(Tokenizer):
             # find the pair with the highest count 
             pair = max(stats, key=stats.get)
             # mint a new token: assign it the next available id  
+            idx = 256 + i 
