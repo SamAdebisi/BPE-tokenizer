74cbf26ba3d4aeff5cd264c9ad2531345b0929ef
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-19 02:26:05 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -56,3 +56,4 @@ class RegexTokenizer(Tokenizer):
             # mint a new token: assign it the next available id  
             idx = 256 + i 
             # replace all occurrences of pair in ids with idx 
+            ids = []
