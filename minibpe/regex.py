54ddd0c18c81decda49540168f0b84528864f3af
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-19 02:20:34 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -50,6 +50,6 @@ class RegexTokenizer(Tokenizer):
             stats = {} 
             for chunk_ids in ids:
                 # passing in stats will update it in place, adding up counts 
-                stats = get_stats(chunk_ids, stats) 
+                get_stats(chunk_ids, stats) 
             # find the pair with the highest count 
             pair = max(stats, stats) 
