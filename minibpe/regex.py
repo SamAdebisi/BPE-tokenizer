c8272cc3ded81042574c1b4aa440d35686a174f0
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-19 02:49:31 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -65,3 +65,5 @@ class RegexTokenizer(Tokenizer):
                 print(f"merge {i+1}/{num_merges}: {pair} -> {idx}) ({vocab[idx]}) had {stats[pair]} occurrences") 
                 
         # save class variables 
+        self.merges = merges # used in encode()
+        self.vocab = vocab   # used in decode()
