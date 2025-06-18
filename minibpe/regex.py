3725a8fae19e524cc56ba9f49d6f413dcfab4219
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-18 10:24:54 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -15,4 +15,5 @@ from .base import Tokenizer, get_stats, merge
 # the main GPT text split patterns, see
 # https://github.com/openai/tiktoken/blob/main/tiktoken_ext/openai_public.py 
 
-
+GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
