3f70d83c2b9df5b6ded9c97444c7a3ebdea972db
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-18 10:23:42 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -1,2 +1,13 @@
+"""
+Minimal (byte-level) Byte Pair Encoding tokenizer.
+
+Algorithmically follows along the GPT tokenizer:
+https://github.com/openai/gpt-2/blob/master/src/encoder.py
+
+Unlike BasicTokenizer:
+- RegexTokenizer handles an optional regex splitting pattern.
+- RegexTokenizer handles optional special tokens.
+"""
+
 import regex as re 
 from .base import Tokenizer, get_stats, merge 
