d1954691468f9dc31e6c6685a0fc7b54fd51b10d
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-18 10:27:23 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -19,4 +19,5 @@ GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
 
 class RegexTokenizer(Tokenizer):
-    pass 
+    
+    
