ffb5cfe31a8329faaad43629993ebf0c4febc97e
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-18 10:20:24 +0300

    Update

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -1,13 +1,3 @@
 import regex as re 
 
-
-# Example pattern and string
-pattern = r"\d+"  # matches one or more digits
-text = "There are 42 apples"
-
-# Test if pattern exists in text
-if re.search(pattern, text):
-    print("Pattern found!")
-else:
-    print("Pattern not found.")
     
