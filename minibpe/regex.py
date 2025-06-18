30a7aea2e073e5febfba32ba707f66523b18a7d0
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-19 02:53:57 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -68,5 +68,5 @@ class RegexTokenizer(Tokenizer):
         self.merges = merges # used in encode()
         self.vocab = vocab   # used in decode() 
         
-    def register_special_token(self, special, idx):
+    def register_special_tokens(self, special, idx):
         
