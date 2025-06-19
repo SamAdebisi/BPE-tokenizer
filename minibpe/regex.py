ddbedd55bdc2857d0556a98835142f782f647677
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-19 03:25:48 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -84,7 +84,7 @@ class RegexTokenizer(Tokenizer):
                 part_bytes.append(self.inverse_special_tokens[idx].encode("utf-8")) 
             else:
                 raise ValueError(f"Invalid token id: {idx}") 
-        text_bytes = b"".join(part_bytes).decode("utf-8", errors="replace")
+        text_bytes = b"".join(part_bytes)
         text 
         return text 
     
