4a80b6420bc2501cf22aa8b8a43192f135012d1a
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-20 00:46:59 +0300

    Update regex.py

--- minibpe/regex.py
+++ minibpe/regex.py
@@ -102,7 +102,7 @@ class RegexTokenizer(Tokenizer):
             # we can detect this terminating case by a membership check 
             if pair not in self.merges:
                 break # nothing else can be merged anymore 
-            # otherwise let's merge the best pair 
+            # otherwise let's merge the best pair (lowest merge index)
             idx = self.merges[pair]
             ids = merge(ids, pair, idx)
         return ids 
