722d1d8ed7c4ebf628b1b17c82155b425e8204fc
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-20 21:10:40 +0300

    Update gpt4.py

--- minibpe/gpt4.py
+++ minibpe/gpt4.py
@@ -1,6 +1,9 @@
-    """
-        Implements the GPT-4 Tokenizer as a light wrapper around the RegexTokenizer. 
-        Note that this is a pretrained tokenizer. By default and inside init(), it loads 
-        the pretrained tokenizer from the `cl100k_base´tokenizer of tiktoken. 
-    """
+"""
+    Implements the GPT-4 Tokenizer as a light wrapper around the RegexTokenizer. 
+    Note that this is a pretrained tokenizer. By default and inside init(), it loads 
+    the pretrained tokenizer from the `cl100k_base` tokenizer of tiktoken. 
+"""
+
+import tiktoken´
+from .regex import RegexTokenizer 
 
