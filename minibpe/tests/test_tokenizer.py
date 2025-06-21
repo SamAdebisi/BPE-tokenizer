e410c81028fa7e55f2ff4ee4369ebd464d728649
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-21 23:43:46 +0300

    Update test_tokenizer.py

--- minibpe/tests/test_tokenizer.py
+++ minibpe/tests/test_tokenizer.py
@@ -18,7 +18,7 @@ def unpack(text):
     # we do this because `pytest -v.` prints the arguments to console, and we don't 
     # want to print the entire contents of the file, it creates a mess. So here we go. 
     if text.startswith("FILE:"):
-        dirname = os.path.dirname(__file__)
+        dirname = os.path.dirname(os.path.abspath(__file__))
         filename = os.path.join(dirname, text[5:])
         text = open(filename, "r", encoding="utf-8").read()
     return text 
