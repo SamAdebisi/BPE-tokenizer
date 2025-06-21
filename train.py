fb23c0139730ac10c6955d6a812ff3b5a274af19
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-21 21:39:16 +0300

    Update train.py

--- train.py
+++ train.py
@@ -8,3 +8,6 @@ from minibpe import BasicsicTokenizer, GPT4Tokenizer, RegexTokenizer
 
 # open some text and train a vocab of 512 tokens 
 text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
+
+# create a directory for models, so we don't pollute the current directory 
+os.makedirs("models", exist_ok=True)
