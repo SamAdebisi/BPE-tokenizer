2c6431771e6b46ba009e71b243650791dbdf0d7f
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-22 01:57:46 +0300

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ The whole thing runs in ~?? seconds on my laptop.
 
 import os 
 import time 
-from minibpe import BasicsicTokenizer, GPT4Tokenizer, RegexTokenizer 
+from minibpe import BasicTokenizer, GPT4Tokenizer, RegexTokenizer 
 
 # open some text and train a vocab of 512 tokens 
 text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
