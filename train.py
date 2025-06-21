14006e6e626ac69b4b63255771c8a987c150c9c5
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-21 21:33:25 +0300

    Update train.py

--- train.py
+++ train.py
@@ -7,4 +7,4 @@ import time
 from minibpe import BasicsicTokenizer, GPT4Tokenizer, RegexTokenizer 
 
 # open some text and train a vocab of 512 tokens 
-text = open("tests/taylorswift.txt")
+text = open("tests/taylorswift.txt", "r")
