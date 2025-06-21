56b941770265995347000b681c295a612277c257
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-21 23:11:42 +0300

    Update test_tokenizer.py

--- minibpe/tests/test_tokenizer.py
+++ minibpe/tests/test_tokenizer.py
@@ -5,3 +5,6 @@ import os
 from minibpe import BasicsicTokenizer, GPT4Tokenizer, RegexTokenizer 
 
 # -------------------------------------------------------------------------------
+# common test data 
+
+# a few strings to test the tokenizers on 
