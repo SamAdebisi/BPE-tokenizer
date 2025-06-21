38797e71b8b2d02dc42ca17b19edab2c3d80e58e
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-21 23:42:16 +0300

    Update test_tokenizer.py

--- minibpe/tests/test_tokenizer.py
+++ minibpe/tests/test_tokenizer.py
@@ -17,4 +17,6 @@ test_strings = [
 def unpack(text):
     # we do this because `pytest -v.` prints the arguments to console, and we don't 
     # want to print the entire contents of the file, it creates a mess. So here we go. 
+    if text.startswith("FILE:"):
+        
     pass 
