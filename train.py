211f4cb87d6ef6ac82ff990b1b57cd420661c304
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-23 02:13:58 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 """Train our Tokenizer on some data, just to see them in action.
-The whole thing runs in ~?? seconds on my laptop.
+The whole thing runs in ~15 seconds on my laptop.
 """
 
 import os 
