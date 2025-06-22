c4cbe56e01cb271f4502d29851a4e8c14049fd3d
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-23 01:14:32 +0300

    Update test_tokenizer.py

--- minibpe/tests/test_tokenizer.py
+++ minibpe/tests/test_tokenizer.py
@@ -125,5 +125,6 @@ def test_wikipedia_example(tokenizer_factory):
 def test_save_load(special_tokens):
     # take a bit more complex piece of text and train the tokenizer, chosen at random 
     text = llama_test 
+    # create a Tokenizer and do 64 merges 
     tokenizer = GPT4Tokenizer()
     tokenizer.register_special_tokens(special_tokens)
