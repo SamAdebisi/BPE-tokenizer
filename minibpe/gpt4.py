a03f43257c8e19a734d3224ae19071c6dc120571
Azeez Adebisi <samadebisi12@gmail.com>
2025-06-23 13:19:21 +0300

    Update gpt4.py

--- minibpe/gpt4.py
+++ minibpe/gpt4.py
@@ -74,7 +74,7 @@ class GPT4Tokenizer(RegexTokenizer):
         # are permuted in a different order. This is completely non-sensical 
         # and probably historical, but therefore we have to deal with it here. 
         self.byte_shuffle = {i: mergeable_ranks[bytes([i])] for i in range(256)}
-        self.inverse_special_tokens = {v: k for k, v in self.byte_shuffle.items()}
+        self.inverse_byte_tokens = {v: k for k, v in self.byte_shuffle.items()}
         # finally register the special tokens 
         self.register_special_tokens(GPT4_SPECIAL_TOKENS) 
         
